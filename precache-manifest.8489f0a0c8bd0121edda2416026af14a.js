3c89d3df5f2b7e35aa84bb228c639adb3a9c98b9
Michael Tandy <mike.tandy.1@gmail.com>
2020-05-08 17:19:16 +0100

    Updates

---
+++ precache-manifest.8489f0a0c8bd0121edda2416026af14a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c43371a99af7b4c89cca71480132e67",
+    "url": "/Drums/index.html"
+  },
+  {
+    "revision": "031ac399fa7bfaab6c8e",
+    "url": "/Drums/static/css/main.2408405b.chunk.css"
+  },
+  {
+    "revision": "39a979b1392d0d565684",
+    "url": "/Drums/static/js/2.39b1334f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Drums/static/js/2.39b1334f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "031ac399fa7bfaab6c8e",
+    "url": "/Drums/static/js/main.67db1cc4.chunk.js"
+  },
+  {
+    "revision": "0c9d7cefb6cf887018ad",
+    "url": "/Drums/static/js/runtime-main.0a90bbfe.js"
+  }
+]);
